3a368c57a8cb000f19b07f82acef9867c543545b
Miray <mirayayerdem@gmail.com>
2021-08-24 23:51:28 +0300

    Adding bg and buttons to ScoreState

--- core/src/com/trammy/game/states/ScoreState.java
+++ core/src/com/trammy/game/states/ScoreState.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
@@ -22,8 +23,8 @@ public class ScoreState extends State{
     private Preferences prefs;
     private int highScore, currentScore;
     private Window popUp;
-    private Texture popUpBg, menuBtnTexture;
-    private ImageButton mainMenuBtn;
+    private Texture popUpBg, menuBtnTexture, newGameBtnTexture;
+    private Image ground, bg;
     private Label highScoreLabel, currentScoreLabel;
     private Label.LabelStyle labelStyle;
     private String highScoreText, currentScoreText, charName, bgName;
@@ -41,10 +42,25 @@ public class ScoreState extends State{
 
         highScoreText = "High Score: " + highScore;
         currentScoreText = "Current Score: " + currentScore;
+
+        if(bgName.equals("")) {
+            bg = new Image(new Texture("bg_grid.png"));
+            ground = new Image(new Texture("ground3.png"));
+
+        }
+        else {
+            bg = new Image(new Texture(bgName));
+            ground = new Image(new Texture("groundjf.png"));
+
+        }
+        ground.setPosition(0, -50);
+
         createWindow();
 
         highScoreLabel.setText(highScoreText);
         currentScoreLabel.setText(currentScoreText);
+
+
     }
 
     public void createWindow(){
@@ -61,21 +77,11 @@ public class ScoreState extends State{
         popUp.setTransform(true);
 
 
-        labelStyle = new Label.LabelStyle(JumpyFruits.font, Color.WHITE);
-        highScoreLabel = new Label(highScoreText, labelStyle);
-        highScoreLabel.setPosition(popUp.getWidth()*0.5f - highScoreLabel.getWidth()/2.75f, popUp.getY() - popUp.getHeight()/2);
-        highScoreLabel.setFontScale(0.65f);
-
-        currentScoreLabel = new Label(currentScoreText, labelStyle);
-        currentScoreLabel.setPosition(popUp.getWidth()*0.5f - highScoreLabel.getWidth()/2.75f, highScoreLabel.getY() - highScoreLabel.getHeight() - 8);
-        currentScoreLabel.setFontScale(0.65f);
-
         menuBtnTexture = new Texture("mainmenubtn.png");
         Drawable menuButtonDrawable = new TextureRegionDrawable(new TextureRegion(menuBtnTexture));
-        mainMenuBtn = new ImageButton(menuButtonDrawable);
+        ImageButton mainMenuBtn = new ImageButton(menuButtonDrawable);
         mainMenuBtn.setSize(menuBtnTexture.getWidth(), menuBtnTexture.getHeight());
         mainMenuBtn.setPosition(cam.position.x - mainMenuBtn.getWidth()/2 , popUp.getY() - mainMenuBtn.getHeight() - 20);
-
         mainMenuBtn.addListener(new InputListener(){
             @Override
             public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
@@ -89,13 +95,43 @@ public class ScoreState extends State{
             }
         });
 
+        newGameBtnTexture = new Texture("newgamebtn4.png");
+        Drawable newGameButtonDrawable = new TextureRegionDrawable(new TextureRegion(newGameBtnTexture));
+        ImageButton newGameBtn = new ImageButton(newGameButtonDrawable);
+        newGameBtn.setSize(newGameBtnTexture.getWidth(),newGameBtnTexture.getHeight());
+        newGameBtn.setPosition(cam.position.x - mainMenuBtn.getWidth()/2 , mainMenuBtn.getY() - newGameBtn.getHeight() - 20);
+        newGameBtn.addListener(new InputListener(){
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("Press a Button");
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                gsm.set(new PlayState(gsm,charName,bgName));
+                return true;
+            }
+        });
+
+        labelStyle = new Label.LabelStyle(JumpyFruits.font, Color.WHITE);
+        highScoreLabel = new Label(highScoreText, labelStyle);
+        highScoreLabel.setPosition(popUp.getWidth()*0.5f - highScoreLabel.getWidth()/2.75f, popUp.getY() - popUp.getHeight()/2);
+        highScoreLabel.setFontScale(0.65f);
+
+        currentScoreLabel = new Label(currentScoreText, labelStyle);
+        currentScoreLabel.setPosition(popUp.getWidth()*0.5f - highScoreLabel.getWidth()/2.75f, highScoreLabel.getY() - highScoreLabel.getHeight() - 8);
+        currentScoreLabel.setFontScale(0.65f);
+
+
 
         popUp.setLayoutEnabled(false);
         popUp.add(highScoreLabel);
         popUp.add(currentScoreLabel);
 
-        stage.addActor(popUp);
+        stage.addActor(bg);
+        stage.addActor(ground);
         stage.addActor(mainMenuBtn);
+        stage.addActor(newGameBtn);
+        stage.addActor(popUp);
         Gdx.input.setInputProcessor(stage);
 
     }
@@ -108,6 +144,8 @@ public class ScoreState extends State{
     @Override
     public void update(float dt) {
         cam.update();
+        Gdx.input.setInputProcessor(stage);
+
     }
 
     @Override
